4d2820d28e1eac3957c5c489b89044d8b301df56
Gill <gillj@0101c302499l.corp.espn.pvt>
2014-03-01 17:58:45 -0500

    initial test case

--- src/main/java/LambdaTest.java
+++ src/main/java/LambdaTest.java
@@ -1,24 +1,25 @@
-var CollectionsAndFiles = new JavaImporter(
-    java.util.Scanner,
-    java.io.File,
-    java.lang.System);
+public class LambdaTest {
 
-with (CollectionsAndFiles) {
-    var cmd = System.getProperties().get('sun.java.command').split(' ');
-    var filename = cmd[cmd.length-1];
-    var file = new File(filename);
-    var url = file.toURI().toURL().openConnection();
-    var fileContents = (new Scanner(url.getInputStream())).useDelimiter("\\Z").next();
-    
-    System.out.println();
-    System.out.println(fileContents);
-    System.out.println();
-    System.out.println('returns:');
-    
-    var dontDoThis = new Packages.org.json.JSONObject();
-    dontDoThis.put('dont_do', 'this');
-    java.lang.System.out.println( dontDoThis.toString() );
-    
-    var doThis = { 'hello' : 'nurse' };
-    System.out.println( JSON.stringify(doThis, null,2) );
+        public static void main(String[] args){
+
+			System.out.println("=== RunnableTest ===");
+  
+			// Anonymous Runnable
+			Runnable r1 = new Runnable(){
+
+				@Override
+				public void run(){
+					System.out.println("Hello world one!");
+				}
+			};
+
+			// Lambda Runnable
+			Runnable r2 = () -> System.out.println("Hello world two!");
+
+			// Run em!
+			r1.run();
+			r2.run();
+
+        };
+
 }
